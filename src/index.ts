0d66b5790f91298b5bbeb1bf12a461fa499dd475
SKB <shubhambhilare4899223@gmail.com>
2026-01-12 13:54:37 +0530

    cors policy

--- src/index.ts
+++ src/index.ts
@@ -18,6 +18,7 @@ const app = express();
 app.use(
   cors({
     origin: [
+      "http://localhost:3000",
       "http://localhost:5500",
       "http://127.0.0.1:5500",
       "https://omkard007.github.io",
